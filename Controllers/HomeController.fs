1d6cce396b6f7dab8c2c349f40e0741ca6ced61e
Tripp Shelnutt <shelnutt77@gmail.com>
2020-03-14 23:57:21 -0400

    Added test

--- Controllers/HomeController.fs
+++ Controllers/HomeController.fs
@@ -7,6 +7,8 @@ open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Configuration
+open System.Net.Http
+open System.Net.Http.Headers
 
 type HomeController (logger : ILogger<HomeController>, configuration : IConfiguration) =
     inherit Controller()
@@ -19,3 +21,14 @@ type HomeController (logger : ILogger<HomeController>, configuration : IConfigur
 
     member this.Error () =
         this.View();
+
+    member this.ApiTest () =
+        async {
+            use client = new HttpClient()
+            client.DefaultRequestHeaders.Accept.Clear()
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue("application/json"))
+            client.DefaultRequestHeaders.Add("Authorization", "BEARER " + configuration.["YNAB:apikey"])
+        
+            let! message = client.GetStringAsync(configuration.["YNAB:URL"] + "/budgets") |> Async.AwaitTask 
+            return this.Content(message)
+        }

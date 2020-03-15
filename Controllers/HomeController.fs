72abfbc0a3b157798492badb1f568ecc23d729fa
Tripp Shelnutt <shelnutt77@gmail.com>
2020-03-15 15:20:54 -0400

    Refactored utility methods and getting transactions

--- Controllers/HomeController.fs
+++ Controllers/HomeController.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Collections.Generic
+open System.Globalization
 open System.Linq
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
@@ -24,11 +25,30 @@ type HomeController (logger : ILogger<HomeController>, configuration : IConfigur
 
     member this.ApiTest () =
         async {
-            use client = new HttpClient()
-            client.DefaultRequestHeaders.Accept.Clear()
-            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue("application/json"))
-            client.DefaultRequestHeaders.Add("Authorization", "BEARER " + configuration.["YNAB:apikey"])
-        
-            let! message = client.GetStringAsync(configuration.["YNAB:URL"] + "/budgets/" + configuration.["YNAB:Budget"] + "/categories") |> Async.AwaitTask 
+            let filterDate = DateTime.ParseExact("2020-2-1", "yyyy-M-d", CultureInfo.InvariantCulture)
+            let! message = this.GetTransactions filterDate
             return this.Content(message)
         }
+
+    member private this.GetTransactions (sinceDate : DateTime) =
+        async {
+            use client = this.BuildHttpClient()
+        
+            let groceriesId = configuration.["YNAB:Groceries"]
+            let requestUrl = this.GetTransactionsByCategoryAndDateRequestUrl groceriesId sinceDate
+            let! message = client.GetStringAsync(requestUrl) |> Async.AwaitTask 
+            return message
+        }
+        
+    member private this.BuildHttpClient () : HttpClient =
+        let client = new HttpClient()
+        client.DefaultRequestHeaders.Accept.Clear()
+        client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue("application/json"))
+        client.DefaultRequestHeaders.Add("Authorization", "BEARER " + configuration.["YNAB:apikey"])
+        client
+
+    member private this.GetTransactionsByCategoryAndDateRequestUrl (categoryId : string) (sinceDate : DateTime) : Uri =
+        let urlBase = configuration.["YNAB:URL"]
+        let budgetId = configuration.["YNAB:Budget"]
+        let sinceDateString = sinceDate.ToString("yyyy-M-d")
+        Uri(sprintf "%s/budgets/%s/categories/%s/transactions?since_date=%s" urlBase budgetId categoryId sinceDateString)
